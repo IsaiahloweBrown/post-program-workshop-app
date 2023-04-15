46d5433b419283102b45e9044a366bf619fb6f63
IsaiahLoweBrown <isaiahlowebrown@gmail.com>
2023-04-15 00:42:14 -0400

    database setup

--- binary-upload-boom-api/sql/schema.sql
+++ binary-upload-boom-api/sql/schema.sql
@@ -17,11 +17,12 @@ CREATE TABLE users (
     last_name VARCHAR(255) NOT NULL,
     username VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) UNIQUE NOT NULL,
     role valid_roles DEFAULT 'user' NOT NULL
 );
 
 -- Create Table posts
-CREATE TABLE posts (
+CREATE TABLE lessons (
     pid SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     image_url VARCHAR(255) NOT NULL,
